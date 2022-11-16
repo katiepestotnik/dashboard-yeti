85a060dae07c2b259f54dfab5f8550fc7b44e18c
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-11-16 16:31:52 -0700

    add dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import {useState} from 'react'
+
 import SideBar from "./components/SideBar";
 import Reviews from "./components/Reviews";
 import Rating from "./components/Rating";
 import Analysis from "./components/Analysis";
 import Visitors from "./components/Visitors";
+import Button from './components/Button'
 
 function App() {
+  const [isOn, setIsOn] = useState(false)
+  const handleClick = (e) => {
+    setIsOn(state => !state)
+    if (isOn) e.target.innerHTML = 'LIGHT'
+    if(!isOn)e.target.innerHTML = 'DARK'
+}
   return (
-    <div className="container">
-    <main>
-      <div className='outer-container'>
-        <SideBar />
+    <div className='full' style={{ backgroundColor: isOn ? '#001242' : '#0f50ff' }}>
+      <button onClick={handleClick}>LIGHT</button>
+      <main>
+        <div className='outer-container'>
+          <SideBar />
         <div className='inner-container'>
           <Reviews />
           <Rating />
           <Analysis />
           <Visitors/>
         </div>
-      </div>
-
-    </main>
+        </div>
+      </main>
     </div>
 
   );
